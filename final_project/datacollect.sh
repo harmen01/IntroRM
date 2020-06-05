038f683ee6d332082628d9613be8de8b3c28af7b
harmen01 <harmen.vogt@gmail.com>
2020-06-05 19:52:46 +0200

    datacollect update

--- final_project/datacollect.sh
+++ final_project/datacollect.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #
-# Descr : 
+# Descr : Can be used on CBS crime dataset to obtain data about
+# Crime suspects and their migration background.
+# It filters out data from certain years and categories.
 # 
 #
 # Usage: ./datacollect.sh FILE
@@ -13,7 +15,11 @@ then
     exit
 fi
 
-#
+# Filters out data for years 2010,2011,2012,2013 and 2014
+# Cuts important fields for research. 
+# Then chooses the migration backgrounds to look at (1012600=Dutch background, 2012605=Migration background).
+# It removes empty fields and selects the year. Shows the upper 2 rows, which contain total yearly data.
+# Also outputs info about data
 echo "Data 2010:"
 echo "MigrationBackground; Period; AmountOfSuspects; AmountOfSuspectsRelative; AmountOfSuspectsReportFiled; AmountOfSuspectsReportFiledRelative"
 cat $TEXT | cut -d ';' -f4,6-7,9-10,12 | grep -E '1012600|2012605' | grep -v -E '\"       .\"|\"\"' | grep 2010JJ00 | head -2
